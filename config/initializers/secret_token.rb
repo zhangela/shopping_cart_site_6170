20d0a1e6904602ae6660fddb697cbe5210253308
Angela Zhang <angelaz@mit.edu>
2013-03-04 00:35:17 -0500

    working sign up/log in page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngelazProj2::Application.config.secret_token = 'a0d209a0ba8bb8fd294756876ec19f696e95e927b8f1fcdcadeecefa322f784008acd39dd4cb5b745d683c2e7b50778d34eac180d11386a68cb8c8b5fc6d6fdb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AngelazProj2::Application.config.session_store :cookie_store, key: '_angelaz_proj2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AngelazProj2::Application.config.session_store :active_record_store
