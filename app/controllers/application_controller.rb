bcd2b75504e4eeebc86ad21777ab42677d5c7fa5
Angela Zhang <angelaz@mit.edu>
2013-03-05 12:28:41 -0500

    minor tweaks to the controller so it creates session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 class ApplicationController < ActionController::Base
-    # protect_from_forgery
 
+    protect_from_forgery
     helper_method :current_user
+    before_filter :add_value_to_session
 
+    # This can be toggled to inspect the session hash.
+    # Run curl -I localhost:3000 (default)
+    #     ruby decode.rb BAh7CEkiD3Nl%3D--eef6f421eb6e51fa8b269b2fd9b406f8f5a3f265 (this will be way longer)
+    # This will decode the cookies and note that decode.rb is in the root dir of this app.
     private
 
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    def add_value_to_session
+    session[:message] = "Hello World!"
     end
+
+    private
+    def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
