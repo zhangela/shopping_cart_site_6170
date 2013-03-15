18e0d9db91e662f348128febe3e62e730b7105ac
Angela Zhang <angelaz@mit.edu>
2013-03-15 17:04:49 -0400

    added caritem quantity validation

--- app/models/cartitem.rb
+++ app/models/cartitem.rb
@@ -2,4 +2,6 @@ class Cartitem < ActiveRecord::Base
   belongs_to :item
   belongs_to :cart
   attr_accessible :quantity, :item, :cart
+
+  validates :quantity, :numericality => { :only_integer => true, :greater_than => 0 }
 end
